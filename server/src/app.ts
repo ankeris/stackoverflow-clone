2b6e4db898053f30b7e1a8f2a17d82c188042200
Juozas Rastenis <jra@novicell.dk>
2019-04-16 16:45:59 +0200

    Cors policy restriction settings added

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,6 +11,12 @@ dotenv.config();
 const app = express();
 const expressRoutes = new ExpressRotuer(app);
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 expressRoutes.init();
 
 // DB Connection
