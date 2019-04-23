ae5a10f6c6753b1dd84b01cbafc36147833f02e5
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-04-23 21:34:26 +0200

    build added to git test

---
+++ client/build/precache-manifest.76c2041b2b20ddf3cf96c5a9f2a215ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "447341e0fc2d474e4d3d",
+    "url": "/static/js/main.fbf91be2.chunk.js"
+  },
+  {
+    "revision": "11a7f5e1c17c229ac84b",
+    "url": "/static/js/2.aad10dd7.chunk.js"
+  },
+  {
+    "revision": "b6d43db88f2ed38ad9df7aa3a66d7400",
+    "url": "/index.html"
+  }
+];
