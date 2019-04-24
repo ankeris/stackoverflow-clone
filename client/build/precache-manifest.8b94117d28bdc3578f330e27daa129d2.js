a22e2ebf1454c83902ae47964507837013d4a67e
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-04-24 21:42:40 +0200

    test

---
+++ client/build/precache-manifest.8b94117d28bdc3578f330e27daa129d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4de6954481d898016d62",
+    "url": "/static/js/main.816a66ff.chunk.js"
+  },
+  {
+    "revision": "11a7f5e1c17c229ac84b",
+    "url": "/static/js/2.aad10dd7.chunk.js"
+  },
+  {
+    "revision": "ee0dce934c5c3520263557722925f46b",
+    "url": "/index.html"
+  }
+];
